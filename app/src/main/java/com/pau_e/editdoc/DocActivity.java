f1d435aeb8bb21dc4258ad86971253d3421bb04c
Penrech <pauenrech94@gmail.com>
2018-02-22 09:53:32 +0100

    App funcional

--- app/src/main/java/com/pau_e/editdoc/DocActivity.java
+++ app/src/main/java/com/pau_e/editdoc/DocActivity.java
@@ -8,9 +8,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class DocActivity extends AppCompatActivity {
 
+    private static final int EDIT_BODY = 1;
+    private static final int EDIT_TITLE = 0;
     private TextView title;
+    private TextView body;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class DocActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doc);
 
         title = findViewById(R.id.title);
+        body = findViewById(R.id.body);
     }
 
     @Override
@@ -31,26 +37,35 @@ public class DocActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.edit_title:
-                editTitle();
+                edit(title.getText().toString(),EDIT_TITLE);
+                break;
+            case  R.id.edit_body:
+                edit(body.getText().toString(),EDIT_BODY);
                 break;
 
         }
         return true;
     }
 
-    private void editTitle() {
+    private  void edit(String text, int requestCode){
         Intent intent = new Intent(this,EditTextActivity.class);
-        intent.putExtra("text",title.getText().toString());
-        startActivityForResult(intent,0);
+        intent.putExtra("text",text);
+        startActivityForResult(intent,requestCode);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode){
-            case 0:
+            case EDIT_TITLE:
                 if (resultCode == RESULT_OK){
                     String text = data.getStringExtra("text");
                     title.setText(text);
+                }
+                break;
+            case EDIT_BODY:
+                if (resultCode == RESULT_OK){
+                    String text = data.getStringExtra("text");
+                    body.setText(text);
                 }
                 break;
         }
